c781282c10917ceaaf064c4341c18a628e9545e9
doquso <doquso6789@gmail.com>
2026-06-09 01:26:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import telebot
 
+from config import BOT_TOKEN
+
+from handlers.start import register as start_register
+from handlers.photo import register as photo_register
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+start_register(bot)
+photo_register(bot)
+
+print("Bot đang chạy...")
+
+bot.infinity_polling(
+    skip_pending=True
+)
